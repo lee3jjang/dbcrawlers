9c1ba7eb16812a1a1e4724ff19f57acc037daac8
Lee Sang-Jin <lee3jjang@gmail.com>
2021-02-11 19:40:41 +0900

    강제로 넣기

--- make.bat
+++ make.bat
@@ -5,6 +5,19 @@ set SPHINXBUILD=sphinx-build
 set SOURCEDIR=sphinx_source
 set BUILDDIR=docs
 
+@REM docs 폴더 비우기
+rmdir /s /q %BUILDDIR%
+
 @REM 빌드
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR%
 
+@REM docs/html 파일 및 폴더들을 docs로 이동
+for %%I in (%BUILDDIR%/html/*) do (
+	move /y %BUILDDIR%\html\%%I %BUILDDIR% >nul 2>nul
+)
+
+for /d %%I in (%BUILDDIR%/html/*) do (
+	move /y %BUILDDIR%\html\%%I %BUILDDIR% >nul 2>nul
+)
+
+rmdir %BUILDDIR%\html
