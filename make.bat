2faba8acfe463e268a7e676312411c74c2b6faa0
Lee Sang-Jin <lee3jjang@gmail.com>
2021-02-11 19:15:52 +0900

    배치 수정 중

--- make.bat
+++ make.bat
@@ -2,8 +2,8 @@
 
 @REM 변수 선언
 set SPHINXBUILD=sphinx-build
-set SOURCEDIR=source
-set BUILDDIR=build
+set SOURCEDIR=sphinx_source
+set BUILDDIR=docs
 
 @REM 빌드
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR%
