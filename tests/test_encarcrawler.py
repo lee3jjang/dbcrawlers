48f5d7ad049ff9b37163ce3a22bbbbf84ac8237d
이상진 <lee3jjang@gmail.com>
2021-02-16 09:58:18 +0900

    일단 고

--- tests/test_encarcrawler.py
+++ tests/test_encarcrawler.py
@@ -10,7 +10,7 @@ def usedcar_price():
     from dbcrawlers.encarcrawler import UsedCarPriceCrawler
 
     url = 'http://www.encar.com/fc/fc_carsearchlist.do?carType=for&searchType=model&TG.R=B#!%7B%22action%22%3A%22(And.Hidden.N._.(C.CarType.N._.Manufacturer.%EB%B2%A4%EC%B8%A0.))%22%2C%22toggle%22%3A%7B%7D%2C%22layer%22%3A%22%22%2C%22sort%22%3A%22ModifiedDate%22%2C%22page%22%3A1%2C%22limit%22%3A20%7D'
-    ucpc = UsedCarPriceCrawler(page_max=3, headless=True)
+    ucpc = UsedCarPriceCrawler(page_max=3, headless=False)
     ucpc.set_url(url)
     price = ucpc.get_usedcar_price()
     yield price
--- tests/test_kofiabondcrawler.py
+++ tests/test_kofiabondcrawler.py
@@ -9,7 +9,7 @@ sys.path.append(str(Path(__file__).parent.parent))
 def interest_rate():
     from dbcrawlers.kofiabondcrawler import InterestRateCrawler
 
-    irc = InterestRateCrawler(headless=True)
+    irc = InterestRateCrawler(headless=False)
     irc.set_daterange('2020-01-01', '2020-12-31')
     irc.set_maturity([1, 3, 5, 10 ,20])
     rate = irc.get_interest_rate()
